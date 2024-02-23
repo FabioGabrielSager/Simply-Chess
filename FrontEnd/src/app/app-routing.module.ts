6f1049ab250e6abd06647a47f6158c084dddfc80
shackgerc <sagerprogramer@gmail.com>
2024-02-22 21:32:43 -0300

    Adding logic to disconnect the user when going from /match to /home

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {inject, NgModule} from '@angular/core';
+import {NavigationEnd, Router, RouterModule, Routes} from '@angular/router';
 import {MenuComponent} from "./components/menu/menu.component";
 import {authGuard} from "./guards/auth/auth.guard";
 import {ConnectingComponent} from "./components/connecting/connecting.component";
+import {SessionService} from "./services/session.service";
+import {filter} from "rxjs";
 
 const routes: Routes = [
   { path: 'home',
@@ -23,4 +25,20 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  private router: Router = inject(Router);
+  private sessionService: SessionService = inject(SessionService);
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(
+      value => {
+        const event = value as NavigationEnd;
+        if(event.url === '/match' && (event.urlAfterRedirects === '/home' || event.urlAfterRedirects === '/')) {
+          this.sessionService.logOut();
+        }
+      }
+    );
+  }
+}
