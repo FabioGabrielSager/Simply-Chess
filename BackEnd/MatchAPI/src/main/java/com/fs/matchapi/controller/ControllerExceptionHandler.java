fb88a196bd2ff1801cd12d47b857013a6a56c098
shackgerc <sagerprogramer@gmail.com>
2024-02-29 16:19:35 -0300

    Adding an exeption handler to manage messagin exceptions

--- BackEnd/MatchAPI/src/main/java/com/fs/matchapi/controller/ControllerExceptionHandler.java
+++ BackEnd/MatchAPI/src/main/java/com/fs/matchapi/controller/ControllerExceptionHandler.java
@@ -8,6 +8,8 @@ import com.fs.matchapi.exceptions.PieceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 
-@ControllerAdvice
+@ControllerAdvice("com.fs.matchapi.controller")
 public class ControllerExceptionHandler {
     @ExceptionHandler(IllegalMovementException.class)
     public ResponseEntity<ErrorApi> handleError(IllegalMovementException exception)
@@ -68,6 +70,24 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ErrorApi> handleError(EntityNotFoundException exception){
         ErrorApi error = buildError(exception.getMessage(), HttpStatus.NOT_FOUND);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @MessageExceptionHandler()
+    public ErrorApi handleMessagingError(Exception exception) {
+        return ErrorApi.builder()
+                .timestamp(String.valueOf(Timestamp.from(ZonedDateTime.now().toInstant())))
+                .error(exception.getLocalizedMessage())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @MessageExceptionHandler()
+    public ErrorApi handleMessagingError(MessagingException exception) {
+        return ErrorApi.builder()
+                .timestamp(String.valueOf(Timestamp.from(ZonedDateTime.now().toInstant())))
+                .error(exception.getLocalizedMessage())
+                .message(exception.getMessage())
+                .build();
     }
 
     private ErrorApi buildError(String message, HttpStatus httpStatus) {
